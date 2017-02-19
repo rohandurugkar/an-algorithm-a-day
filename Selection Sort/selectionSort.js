35d5873c04bae517b8acfbb390c520650fb2a868
Rohan Durugkar <rohandurugkar@gmail.com>
2017-02-20 00:33:35 +0200

    Completed selection sort

--- Selection Sort/selectionSort.js
+++ Selection Sort/selectionSort.js
@@ -1,16 +1,32 @@
-var swap = function(array, firstIndex, secondIndex){
+var swap = function(array, firstIndex, secondIndex) {
   var temp = array[firstIndex];
   array[firstIndex] = array[secondIndex];
   array[secondIndex] = temp;
-}
-var indexOfMinimum = function(array, startIndex){
+};
+
+var indexOfMinimum = function(array, startIndex) {
   var minIndex = startIndex;
   var minValue = array[startIndex];
 
-  for(var i = minIndex + 1; i < array.length; i++){
-    if(array[i] < minValue){
+  for(var i = minIndex + 1; i < array.length; i++) {
+    if(array[i] < minValue) {
       minIndex = i;
+      minValue = array[i];
     }
   }
-  return i;
-}
+  return minIndex;
+};
+
+var selectionSort = function(array) {
+  var tempMin;
+
+  for(var j = 0; j < array.length; j++) {
+    tempMin = indexOfMinimum(array, j);
+    swap(array, j, tempMin);
+  }
+};
+
+var array = [8, 22, 3, 9, 7, 0, -87];
+selectionSort(array);
+
+alert("The sorted array is " + array);
