fa21d46bdef2d5bf060d2e6046a40fcce40656fc
Rohan Durugkar <rohandurugkar@gmail.com>
2017-02-22 22:01:47 +0200

    Completed recursive powers algorithm

--- Recursive Algorithms/Recursive Powers/recursivePowers.js
+++ Recursive Algorithms/Recursive Powers/recursivePowers.js
@@ -1,6 +1,21 @@
+var power = function(x, n) {
+  //base case
+  if(n === 0) {
+    return 1;
+  }
+  //recursive case, n is negative
+  if(n < 0){
+    return 1/power(x, -n);
+  }
+  //recursive case, n is odd
+  if(n % 2 !== 0) {
+    return power(x, n-1) * x;
+  }
+  //recursive case, n is even
+  if(n % 2 === 0){
+    var y = power(x, n/2);
+    return y * y;
+  }
+}
 
-
-var array = [8, 22, 3, 9, 7, 0, -87];
-selectionSort(array);
-
-alert("The sorted array is " + array);
+alert("The result is " + power(2, 8));
