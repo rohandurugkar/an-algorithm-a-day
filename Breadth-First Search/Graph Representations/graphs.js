91b2b2b026aed3a34c9e54a79b535d1330ae3425
Rohan Durugkar <rohandurugkar@gmail.com>
2017-02-26 21:20:45 +0200

    Added placeholders for BFS algorithm

---
+++ Breadth-First Search/Graph Representations/graphs.js
@@ -0,0 +1,20 @@
+//a graph with six vertices, indexed 0-5
+
+//edge list
+var edgeList = [ [0, 2], [1, 3], [2, 3], [2, 4], [3, 5], [4, 5] ];
+
+//adjacency matrix
+var adjMatrix = [[0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 1, 1, 0],
+                 [0, 0, 0, 0, 0, 1],
+                 [0, 0, 0, 0, 0, 1],
+                 [0, 0, 0, 0, 0, 0]];
+
+//adjacency list
+var adjList = [[2],
+               [3],
+               [3, 4],
+               [5],
+               [5],
+               []];
